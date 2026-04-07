309a6663e97a3d85b1314983514e661bf9b7e5eb
bleu-fire <oussamannajag@gmail.com>
2026-04-07 08:37:36 +0000

    first push

---
+++ script.js
@@ -0,0 +1,12 @@
+  const btn = document.getElementById("button");
+
+  btn.addEventListener("click", () => {
+
+    document.body.style.background = "darkred";
+
+
+    
+    setTimeout(() => {
+      document.body.style.background = "black";
+    }, 200);
+  });
